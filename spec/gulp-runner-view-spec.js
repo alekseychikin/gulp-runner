696e04750c77c8018620dda13ee37114da991269
Aleksei Chikin <mail@makingoff.name>
2017-07-20 20:32:05 +0300

    Adds task runner

---
+++ spec/gulp-runner-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GulpRunnerView from '../lib/gulp-runner-view';
+
+describe('GulpRunnerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
